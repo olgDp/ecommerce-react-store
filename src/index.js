1cb4ba0922e359676896d8410d2e2df165c89ad4
Oleg Dneprovsky <oleg.nhatrang@gmail.com>
2020-02-24 18:05:14 +0200

    I have added some routing to the app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import Routes from "routes";
 import "./index.css";
-import App from "./App";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes />
+    </Router>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
